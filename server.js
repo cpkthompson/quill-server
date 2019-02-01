64d80e6b3886ed9675b651c67f329aa6b4be989f
Charles Thompson <cpkthompson@gmail.com>
2019-02-01 01:50:59 +0000

    addition of editorial dashboard and quill server

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
-app.post('/', function(req, res) {
+app.post('/delta', function (req, res) {
     var caseHtmlString = req.body.case;
     res.json(convertHtmlToDelta(caseHtmlString))
+});
+app.post('/html', function (req, res) {
+    var caseDelta = req.body.delta;
+    res.json(convertDeltaToHtml(caseDelta))
 });
