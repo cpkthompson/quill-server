6092767ac142127ca3ed53f10fed1652f0b5cffa
Charles Thompson <cpkthompson@gmail.com>
2019-02-12 05:39:01 +0000

    heroku deploy setup

--- server.js
+++ server.js
@@ -8,9 +8,16 @@ var app = express();
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, () => {
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
+
 
 app.post('/delta', function (req, res) {
     var caseHtmlString = req.body.case;
